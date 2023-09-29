0a25ed36b8c17f7edc2a824caa2aa74980a212dc
Mohan <mohanavel15@protonmail.com>
2023-09-29 23:12:33 +0530

    major changes to ui and added skills section

--- src/App.tsx
+++ src/App.tsx
@@ -46,8 +46,27 @@ const App: Component = () => {
 					<span class='text-lg'>Highly motivated and dedicated Full-Stack Developer with a passion for building innovative web applications. Committed to delivering high-quality code and continuously improving skills to drive impactful results.</span>
 				</div>
 			</div>
-			<img src='/assets/split2.svg' class='w-full min-w-full' />
-			<div class='relative flex flex-col items-center justify-center bg-[#C62368]'>
+			<img src='/assets/split2.svg' class='w-full' />
+			<div class='relative flex flex-col items-center justify-center bg-[#FFF] text-black'>
+				<span class='text-4xl my-12'>Skills</span>
+				<div class='grid gap-8 grid-cols-2 md:grid-cols-4 lg:grid-cols-8 justify-center'>
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" class="h-16 w-16" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" class="h-16 w-16" />
+				</div>
+			</div>
+			<img src='/assets/split3.svg' class='w-full' />
+			<div class='relative flex flex-col items-center justify-center bg-[#0066FF] text-black'>
 				<span class='text-4xl my-12'>Projects</span>
 				<div class='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center'>
 					<For each={projects}>
@@ -55,7 +74,7 @@ const App: Component = () => {
 					</For>
 				</div>
 			</div>
-			<div class='w-full h-16 bg-[#C62368]'></div>
+			<div class='w-full h-16 bg-[#0066FF]'></div>
 		</div>
 	);
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,13 +11,13 @@ export type CardProps = {
 
 export default function Card(props: CardProps) {
 	return (
-		<div class="relative p-4 h-96 w-80 rounded-lg flex flex-col gap-2 text-black bg-amber-500">
+		<div class="relative p-4 h-96 w-80 rounded-lg flex flex-col gap-2 text-white bg-zinc-700">
 			<span class="font-semibold">{props.type}</span>
 			<img class="w-full" src={props.imgsrc} />
 			<span class="font-medium">{props.name}</span>
-			<div class="flex items-center justify-center">
-				<button onClick={() => window.open(props.github)} class="w-24 border-2 border-black rounded px-1 mr-1 shadow-md shadow-black hover:shadow-none">GitHub</button>
-				{props.demo && <button onClick={() => window.open(props.demo)} class="w-24 border-2 border-black rounded px-1 mr-1 shadow-md shadow-black hover:shadow-none">Demo</button>}
+			<div class="flex items-center justify-center text-black">
+				<button onClick={() => window.open(props.github)} class=" bg-white w-24 rounded px-1 mr-1">GitHub</button>
+				{props.demo && <button onClick={() => window.open(props.demo)} class="bg-white w-24 rounded px-1 mr-1">Demo</button>}
 			</div>
 			<div class="relative w-full flex flex-wrap gap-2 items-center">
 				<For each={props.tags}>{(tag) =>
