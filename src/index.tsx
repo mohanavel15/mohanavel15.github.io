f4dd50fee1b235422b91462e767f3ea10d808431
Mohan <mohanavel15@protonmail.com>
2024-08-23 19:24:09 +0530

    removed current project

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-/* @refresh reload */
-import './index.css';
-import { render } from 'solid-js/web';
-
-import App from './App';
-
-const root = document.getElementById('root');
-
-render(() => <App />, root!);
