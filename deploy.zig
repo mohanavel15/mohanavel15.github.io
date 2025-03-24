c23252eef4de06a93cf8de9be935ab14203a86ac
Mohan <mohanavel15@protonmail.com>
2025-03-24 17:40:23 +0530

    added zig-out temorarily for github pages deployment

--- deploy.zig
+++ deploy.zig
@@ -10,10 +10,12 @@ fn gh_pages(step: *std.Build.Step, _: std.Build.Step.MakeOptions) anyerror!void 
     const allocator = step.owner.allocator;
 
     try execute_cmd(step, "git checkout --orphan gh-pages", allocator);
+    try execute_cmd(step, "rm -rf public", allocator);
     try execute_cmd(step, "mv zig-out public", allocator);
-    try execute_cmd(step, "git add public", allocator);
-    try execute_cmd(step, "git commit -m \"Deploy_to_gh-pages\"", allocator);
+    try execute_cmd(step, "git add public/*", allocator);
+    try execute_cmd(step, "git commit -m \"deploy_to_gh-pages\"", allocator);
     try execute_cmd(step, "git checkout main", allocator);
+    try execute_cmd(step, "rm -rf zig-out", allocator);
     try execute_cmd(step, "mv public zig-out", allocator);
 }
 
